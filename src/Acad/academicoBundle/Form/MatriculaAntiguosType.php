4075e54745f790671326dd3ea996d7e18ac180cf
mariofernip <mariofernip@hotmail.com>
2014-07-08 15:39:16 -0500

    Busqueda Form Type.

---
+++ src/Acad/academicoBundle/Form/MatriculaAntiguosType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Acad\academicoBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+class MatriculaAntiguosType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+       
+        $builder
+            ->add('cedula');
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acad\academicoBundle\Entity\Estudiante'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'acad_academicobundle_matricula';
+    }
+}
