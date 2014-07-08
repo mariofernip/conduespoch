7ca6cd0af8371c21063bf49b1bcbfd0882000f1a
mariofernip <mariofernip@hotmail.com>
2014-07-08 15:38:04 -0500

    Matricula Form Type.

--- src/Acad/academicoBundle/Form/MatriculaType.php
+++ src/Acad/academicoBundle/Form/MatriculaType.php
@@ -2,44 +2,43 @@
 
 namespace Acad\academicoBundle\Form;
 
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
-class MatriculaType extends AbstractType
-{
-        /**
+class MatriculaType extends AbstractType {
+
+    /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
         $builder
-            ->add('periodo')                                    
-            ->add('nivel')                                    
-            ->add('seccion', 'choice', array('choices'   => array('Diurna' => 'Diurna', 'Vespertina' => 'Vespertina', 'Nocturna' => 'Nocturna'),  'required'  => false))                        
-            ->add('estudiante','entity',array(
-                    'class'=> 'academicoBundle:Estudiante',
-                    'query_builder'=> function(EntityRepository $er){
-                    return $er->createQueryBuilder('e')
-                    ->select('e')        
-                    ->join('academicoBundle:Inscripcion', 'i', 'WITH', 'e.id =i.estudiante')                            
-                    ->join('administrativoBundle:Periodo', 'p', 'WITH', 'p.id = i.periodo')           
-                    ->where('i.estado = 1 and p.estado = 1')
-                            
-                    ;                    
-                }
-                ))  
-            ->add('observaciones')                
+                ->add('seccion', 'choice', array('choices' => array('Diurna' => 'Diurna', 'Vespertina' => 'Vespertina', 'Nocturna' => 'Nocturna'), 'required' => false))
+                ->add('observaciones')
+                ->add('nivel')
+                ->add('periodo', 'entity', array(
+                    'class' => 'administrativoBundle:Periodo',
+                    'query_builder' => function(EntityRepository $er) {
+                        return $er->createQueryBuilder('p')
+                                ->select('p')
+                                ->where('p.estado = 1')
+
+                        ;
+                    }
+                ))
+
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Acad\academicoBundle\Entity\Matricula'
         ));
@@ -48,8 +47,9 @@ class MatriculaType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'acad_academicobundle_matricula';
     }
+
 }
+
