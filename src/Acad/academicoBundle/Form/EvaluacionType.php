51f7afb9e65c4e6501e4c730e26233bbf31de588
mariofernip <mariofernip@hotmail.com>
2014-07-24 16:17:06 -0500

    Modificacion formulario Evaluacion

--- src/Acad/academicoBundle/Form/EvaluacionType.php
+++ src/Acad/academicoBundle/Form/EvaluacionType.php
@@ -5,6 +5,7 @@ namespace Acad\academicoBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class EvaluacionType extends AbstractType
 {
@@ -16,8 +17,17 @@ class EvaluacionType extends AbstractType
     {
         $builder      
             ->add('promedio')
-            ->add('descripcion')            
-            ->add('mesevaluacion')
+            ->add('descripcion')    
+             ->add('mesevaluacion', 'entity',array(
+                    'class'=> 'administrativoBundle:MesEvaluacion',
+                    'query_builder'=> function(EntityRepository $er){
+                     return $er->createQueryBuilder('e')
+                     ->join('e.mes', 'm')        
+                     ->where('e.estado = true')                            
+            
+                    ;                    
+                }
+                ))    
             ->add('tiponota')
         ;
     }
